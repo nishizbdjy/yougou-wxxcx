6a25ccab5bc5033020e01d020ed4282715406e10
lbw <2659154921@qq.com>
2020-02-28 23:45:32 +0800

    底部的返回

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,6 +8,8 @@ Page({
     navData: []
     //楼层数据
     floorData: []
+    //返回顶部的显示
+    inshow: false
   },
   onLoad() {
     //获取轮播图数据
@@ -57,6 +59,32 @@ Page({
       this.setData({
         floorData: message
       })
+    })
+  },
+  // 返回顶部
+  fanhuidb() {
+    wx.pageScrollTo({
+      scrollTop: 0,
+      duration: 300
+    })
+  },
+  //页面滚动事件
+  onPageScroll(e) {
+    //不能频繁调用setData
+    //将当前的状态赋值
+    let inshow = this.data.inshow
+    if (e.scrollTop > 150) {
+      //赋true
+      inshow = true
+    } else {
+      //赋false
+      inshow = false
+    }
+    //判断值是否改变，没改变就return
+    if(this.data.inshow === inshow) return
+    //改变值
+    this.setData({
+      inshow: inshow
     })
   }
 })
