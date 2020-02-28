bb91604c976872289929bf3eb60b4d48843205e6
lbw <2659154921@qq.com>
2020-02-28 18:14:08 +0800

    分类导航栏,分类的跳转

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,8 @@ Page({
   data() {
     //轮播图数据
     carousel: []
+    //商品导航栏数据
+    navData: []
   },
   onLoad() {
     //获取轮播图数据
@@ -17,6 +19,28 @@ Page({
       //赋值
       this.setData({
         carousel: message
+      })
+    })
+    // 获取商品导航栏数据
+    request({
+      url: '/home/catitems'
+    }).then(res => {
+      //解构数据
+      const {
+        message
+      } = res.data
+      //改造数据添加路由
+      let newData = message.map((v, i) => {
+        // 第一个分类
+        if (i === 0) {
+          console.log(v)
+          v.url = '/pages/category/index'
+        }
+        return v
+      })
+      //赋值
+      this.setData({
+        navData: newData
       })
     })
   }
