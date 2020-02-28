be3db35e007702294942c7d5f7d4b9773fd6e62d
lbw <2659154921@qq.com>
2020-02-28 17:35:29 +0800

    搜索部分和轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,24 @@
+//引入封装的request
+import request from '../../utils/request.js'
 Page({
-  
+  data() {
+    //轮播图数据
+    carousel: []
+  },
+  onLoad() {
+    //获取轮播图数据
+    request({
+      url: '/home/swiperdata'
+    }).then(res => {
+      console.log(res)
+      //解构数据
+      const {
+        message
+      } = res.data
+      //赋值
+      this.setData({
+        carousel: message
+      })
+    })
+  }
 })
