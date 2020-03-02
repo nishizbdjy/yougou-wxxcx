28e2a0442b51e70a9da9ca752a9498d19ad2c805
lbw <2659154921@qq.com>
2020-03-02 16:34:29 +0800

    输入框自定义组件的样式定义及使用

--- components/search/index.js
+++ components/search/index.js
@@ -4,9 +4,14 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    context: {
+      type: String,
+      //默认值
+      value: '搜索'
+    }
   },
-
+  //定义传递类名
+  externalClasses: ['beijingyans'],
   /**
    * 组件的初始数据
    */
