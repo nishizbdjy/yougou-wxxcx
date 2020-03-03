3a62d070f0d6e2620d54e7e19b63a03543cee389
lbw <2659154921@qq.com>
2020-03-03 17:29:29 +0800

    搜索输入时请求数据，并点击取消时清空输入

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,42 @@
 // pages/search/index.js
+import request from '../../utils/request.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    //当前输入框的值
+    inputValue: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //input事件
+  inputshijian(e) {
+    //获取赋值当前输入框的值
+    this.setData({
+      inputValue: e.detail.value
+    })
+    //搜索建议查询
+    request({
+      url: '/goods/qsearch',
+      data: {
+        query: this.data.inputValue
+      }
+    }).then(res => {
+      console.log(res)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //点击取消按钮事件
+  cancel() {
+    //将当前输入框绑定的值清空
+    this.setData({
+      inputValue: ''
+    })
   }
 })
