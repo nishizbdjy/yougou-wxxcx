a444b7b8536cd013595ce2b852c74b6aa66ad68d
lbw <2659154921@qq.com>
2020-03-03 17:54:45 +0800

    完成输入框请求的防抖节流，(必须请求用户输入最后一次的值)

--- pages/search/index.js
+++ pages/search/index.js
@@ -7,7 +7,11 @@ Page({
    */
   data: {
     //当前输入框的值
-    inputValue: ''
+    inputValue: '',
+    //当前请求的状态
+    nowState: false,
+    //存储上次请求的关键字
+    lastKeyword: ''
   },
 
   /**
@@ -16,6 +20,34 @@ Page({
   onLoad: function(options) {
 
   },
+  //封装的搜索推荐关键字
+  searchKeyword() {
+    //判断状态是否为false
+    if (this.data.nowState == false) {
+      //进来后将门关上
+      this.setData({
+        nowState: true,
+        lastKeyword: this.data.inputValue
+      })
+      request({
+        url: '/goods/qsearch',
+        data: {
+          query: this.data.inputValue
+        }
+      }).then(res => {
+        console.log(res)
+        //将状态设为false
+        this.setData({
+          nowState: false
+        })
+        //判断当前请求的关键字与当前输入框的值是否一致
+        if (this.data.lastKeyword !== this.data.inputValue) {
+          //不一致就再请求一次
+          this.searchKeyword()
+        }
+      })
+    }
+  },
   //input事件
   inputshijian(e) {
     //获取赋值当前输入框的值
@@ -23,20 +55,13 @@ Page({
       inputValue: e.detail.value
     })
     //搜索建议查询
-    request({
-      url: '/goods/qsearch',
-      data: {
-        query: this.data.inputValue
-      }
-    }).then(res => {
-      console.log(res)
-    })
+    this.searchKeyword()
   },
   //点击取消按钮事件
   cancel() {
     //将当前输入框绑定的值清空
     this.setData({
-      inputValue: ''
+      inputValue: '',
     })
   }
 })
