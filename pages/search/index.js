c4a873a568ffab7da57a320063d86faa1d1e9b6f
lbw <2659154921@qq.com>
2020-03-03 20:54:51 +0800

    完成搜索历史的存储及展示跳转

--- pages/search/index.js
+++ pages/search/index.js
@@ -13,14 +13,25 @@ Page({
     //存储上次请求的关键字
     lastKeyword: '',
     //推荐列表
-    suggestList: []
+    suggestList: [],
+    //搜索历史
+    search_history: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    //获取历史记录
+    let arr = wx.getStorageSync('history')
+    //判断arr是否等于数组
+    if (!Array.isArray(arr)) {
+      arr = []
+    }
+    //赋值
+    this.setData({
+      search_history: arr
+    })
   },
   //封装的搜索推荐关键字
   searchKeyword() {
@@ -58,6 +69,14 @@ Page({
     this.setData({
       inputValue: e.detail.value
     })
+    //判断有值才搜索
+    if (!this.data.inputValue) {
+      //将最后一次的数组清空(之前不用是因为它请求了空数据赋值,加了判断有值才搜索,要加上 )
+      this.setData({
+        suggestList: []
+      })
+      return
+    }
     //搜索建议查询
     this.searchKeyword()
   },
@@ -68,6 +87,30 @@ Page({
       //将推荐列表数据清空
       suggestList: []
     })
+  },
+  //用户回车事件
+  huicheEnter() {
+    //有值才执行
+    if (!this.data.inputValue) return
+    //将新数据添加到获取的数组前面
+    this.data.search_history.unshift(this.data.inputValue)
+    //数组去重
+    let arr = [...new Set(this.data.search_history)]
+    //将搜索历史存储到本地
+    wx.setStorageSync('history', arr)
+    //用户搜索回车跳转到商品列表页
+    wx.redirectTo({
+      url: '/pages/goods_list/index?keyword=' + this.data.inputValue
+    })
+  },
+  //点击清空历史事件
+  qingkongls() {
+    //清空展示的
+    this.setData({
+      search_history: []
+    })
+    //清空本地
+    wx.removeStorageSync('history')
   },
   //点击取消按钮事件
   cancel() {
