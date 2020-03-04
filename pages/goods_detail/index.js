a22933e446baa3d3a3a2d7ee0a11aa70fb703f18
lbw <2659154921@qq.com>
2020-03-04 23:26:50 +0800

    完成详情页的渲染

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,70 +1,49 @@
 // pages/goods_detail/index.js
+import request from '../../utils/request.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    image: [
-      'https://img12.360buyimg.com/babel/s1180x940_jfs/t1/85809/25/13704/100868/5e5c6308E3d591a94/bd7760172e73b017.jpg.webp',
-      'https://img11.360buyimg.com/pop/s1180x940_jfs/t1/102163/12/13488/56776/5e58db6dE9cee22b4/8793970a36bb5957.jpg.webp',
-      'https://img12.360buyimg.com/pop/s1180x940_jfs/t1/85244/12/13467/80338/5e578a17E5a8ede53/733823992e9dfcbb.jpg.webp'
-    ]
+    //存储详情信息的数组
+    detailsData: [],
+    //tab索引
+    tabindex: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    console.log(options)
+    const {
+      id
+    } = options
+    //说去商品详情信息
+    request({
+      url: '/goods/detail',
+      data: {
+        goods_id: id
+      }
+    }).then(res => {
+      console.log(res)
+      let {
+        message
+      } = res.data
+      //将价格添加小数
+      message.goods_price = Number(message.goods_price).toFixed(2)
+      //赋值
+      this.setData({
+        detailsData: message
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
+  //点击tab更换索引
+  handletab(e) {
+    //更换当前索引
+    this.setData({
+      tabindex: e.currentTarget.dataset.index
+    })
   }
 })
