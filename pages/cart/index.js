39b34096f797391135acc6776023feaf1088fc20
lbw <2659154921@qq.com>
2020-03-06 18:52:41 +0800

    完成购物车获取用户地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,62 +5,44 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    //地址信息
+    usersite: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    //从本地获取地址
+    this.setData({
+      usersite: wx.getStorageSync('site') || {}
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //获取用户地址
+  huoqudizhi() {
+    wx.chooseAddress({
+      success: (res) => {
+        console.log(res)
+        //赋值
+        this.setData({
+          usersite: res
+        })
+        //存储到本地
+        wx.setStorageSync('site', res)
+      },
+      fail: () => {
+        wx.showToast({
+          title: '获取地址失败',
+          icon: 'none'
+        })
+      }
+    })
   },
-
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
