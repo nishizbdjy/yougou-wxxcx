138683da67d68abcaeab176d12dc5802fc6756e6
lbw <2659154921@qq.com>
2020-03-10 18:24:43 +0800

    我的中心页

--- pages/personal/index.js
+++ pages/personal/index.js
@@ -34,39 +34,14 @@ Page({
       })
     }
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
+  shouhuosite() {
+    //调用收货地址
+    wx.chooseAddress()
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
+  //联系客服
+  lianxikf() {
+    wx.makePhoneCall({
+      phoneNumber: '110'
+    })
   }
 })
